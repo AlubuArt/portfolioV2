3503c7c13ee376d005deba1a04151c6b24a594c8
AlubuArt <jc@jcvisueldesign.dk>
2021-12-07 20:25:18 +0100

    rm onclick from test

--- ui/components/3-organisms/ProjectDescription/ProjectDescription.test.tsx
+++ ui/components/3-organisms/ProjectDescription/ProjectDescription.test.tsx
@@ -4,7 +4,7 @@ import { render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 it('renders a h1, h2 and a CTA primary, with text "test"', () => {
-    render(<ProjectDescription list={['react', 'html', 'css']} title={"test"} description={"test"} onClick={() => {}}></ProjectDescription>)
+    render(<ProjectDescription list={['react', 'html', 'css']} title={"test"} description={"test"}></ProjectDescription>)
     expect(screen.getByRole('ProjectDescription')).toHaveClass("ProjectDescription_container")
     expect(screen.getByRole('heading', {level: 2})).toHaveTextContent("test")
     expect(screen.getByRole('heading',{level: 3})).toHaveTextContent("test")
