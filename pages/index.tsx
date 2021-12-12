624f63dd3e1710d66116138e6826695b9d3d72dc
AlubuArt <jc@jcvisueldesign.dk>
2021-12-12 07:24:56 +0100

    added hamburger menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,9 @@ import { ProjectHeader } from "../ui/components/3-organisms/ProjectHeader";
 import { ProjectDescription } from "../ui/components/3-organisms/ProjectDescription";
 import { Gallery } from "../ui/components/3-organisms/Gallery";
 import { ProjectCard } from "../ui/components/2-molecules/ProjectCard";
+import { Hamburger} from '../ui/components/1-atoms/Hamburger';
+
+
 
 const Home: NextPage = () => {
   let [buttonName, setButtonName] = useState("Button");
@@ -20,38 +23,44 @@ const Home: NextPage = () => {
     "https://firebasestorage.googleapis.com/v0/b/jc-portofolio-93953.appspot.com/o/Sk%C3%A6rmbillede%202020-12-28%20kl.%2013.22.18.png?alt=media&token=4449a9f8-2b9f-4585-818b-ba804f5a75d9";
 
   return (
+    <>
+<Hamburger></Hamburger>
+    
     <div style={{ maxWidth: "800px", marginRight: "auto", marginLeft: "auto" }}>
-      <div>
-        <ProjectHeader
-          title="sponsormatch.dk"
-          description="Get Things Done er en klassisk todo applikation, hvor man kan tilføje, slette og ændre status på todos, samt sætte deadlines for hver todo."
-          onClick={handleClick}
-        ></ProjectHeader>
-      </div>
 
-      <div style={{ maxWidth: "800px" }}>
-        <Gallery
-          slides={[
-            "https://firebasestorage.googleapis.com/v0/b/jc-portofolio-93953.appspot.com/o/sponsormatch.png?alt=media&token=a379dfa2-e761-49ff-934a-4fc84d15e373",
-            "https://firebasestorage.googleapis.com/v0/b/jc-portofolio-93953.appspot.com/o/Sk%C3%A6rmbillede%202021-09-13%20kl.%2019.26.38.png?alt=media&token=7410d19d-0305-4348-9523-0a37a376ebd2",
-          ]}
-        ></Gallery>
-      </div>
-      <div>
-        <ProjectDescription
-          title="about this project"
-          description="Get Things Done er en klassisk todo applikation, lavet med React og Typescript, stylet med CSS modules og bruger React Context API til at håndtere state. Med applikationen kan man tilføje, slette og ændre status på todos, samt sætte deadlines for hver todo."
-          list={[]}
-        ></ProjectDescription>
-      </div>
-      <div>
-        <ProjectDescription
-          title="tech used"
-          description="in this project the following technologies was used"
-          list={["react", "firebase", "Bootstrap", "NextJS"]}
-        ></ProjectDescription>
-      </div>
-    </div>
+        
+
+        <div>
+          <ProjectHeader
+            title="sponsormatch.dk"
+            description="Get Things Done er en klassisk todo applikation, hvor man kan tilføje, slette og ændre status på todos, samt sætte deadlines for hver todo."
+            onClick={handleClick}
+          ></ProjectHeader>
+        </div>
+
+        <div style={{ maxWidth: "800px" }}>
+          <Gallery
+            slides={[
+              "https://firebasestorage.googleapis.com/v0/b/jc-portofolio-93953.appspot.com/o/sponsormatch.png?alt=media&token=a379dfa2-e761-49ff-934a-4fc84d15e373",
+              "https://firebasestorage.googleapis.com/v0/b/jc-portofolio-93953.appspot.com/o/Sk%C3%A6rmbillede%202021-09-13%20kl.%2019.26.38.png?alt=media&token=7410d19d-0305-4348-9523-0a37a376ebd2",
+            ]}
+          ></Gallery>
+        </div>
+        <div>
+          <ProjectDescription
+            title="about this project"
+            description="Get Things Done er en klassisk todo applikation, lavet med React og Typescript, stylet med CSS modules og bruger React Context API til at håndtere state. Med applikationen kan man tilføje, slette og ændre status på todos, samt sætte deadlines for hver todo."
+            list={[]}
+          ></ProjectDescription>
+        </div>
+        <div>
+          <ProjectDescription
+            title="tech used"
+            description="in this project the following technologies was used"
+            list={["react", "firebase", "Bootstrap", "NextJS"]}
+          ></ProjectDescription>
+        </div>
+      </div></>
   );
 };
 
--- ui/components/1-atoms/Hamburger/Hamburger.tsx
+++ ui/components/1-atoms/Hamburger/Hamburger.tsx
@@ -1,13 +1,69 @@
 import React from "react";
+import { slide as Menu } from "react-burger-menu";
+import Link from "next/link";
+import { Autoplay } from "swiper";
+import { Paragraph } from "../Paragraph";
 
+//TODO: move this to css module
+var styles = {
+  bmBurgerButton: {
+    position: "fixed",
+    width: "36px",
+    height: "30px",
+    right: "36px",
+    top: "36px",
+  },
+  bmBurgerBars: {
+    background: "#373a47",
+  },
+  bmBurgerBarsHover: {
+    background: "#a90000",
+  },
+  bmCrossButton: {
+    height: "24px",
+    width: "64px",
+  },
+  bmCross: {
+    background: "#bdc3c7",
+  },
+  bmMenuWrap: {
+    position: "fixed",
+  },
+  bmMenu: {
+    background: "#1b1c24",
+    padding: "2.5em 1.5em 0",
+    fontSize: "1.15em",
+  },
+  
+  bmItemList: {
+    color: "#b8b7ad",
+    padding: "0.8em",
+    height: "95%",
+  },
+  bmItem: {
+    display: "inline-block",
+  },
+  bmOverlay: {
+    background: "rgba(0, 0, 0, 0.3)",
+  },
+};
 
-interface HamburgerProps {
-    logo?: string;
-    menuItems: [];
-}
-
-export const Hamburger: React.FC<HamburgerProps> = ({logo, menuItems}) => (
-    <>
-    
-    </>
-)
+export const Hamburger: React.FC = () => (
+  <Menu right styles={styles}>
+    <Link href="/about" passHref>
+      <Paragraph tag="p">about me</Paragraph>
+    </Link>
+    <Link href="/projects" passHref>
+      <Paragraph tag="p">projects</Paragraph>
+    </Link>
+    <Link href="/experience" passHref>
+      <Paragraph tag="p">experience</Paragraph>
+    </Link>
+    <Link href="/education" passHref>
+      <Paragraph tag="p">education</Paragraph>
+    </Link>
+    <Link href="/contact" passHref>
+      <Paragraph tag="p">contact</Paragraph>
+    </Link>
+  </Menu>
+);
