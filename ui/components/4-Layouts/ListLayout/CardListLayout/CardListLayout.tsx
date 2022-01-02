a1b6ff40d5b48df0f6261806fc53457440554c62
AlubuArt <jc@jcvisueldesign.dk>
2022-01-02 09:50:30 +0100

    responsive design styliong done - 1200px

--- ui/components/4-Layouts/ListLayout/CardListLayout/CardListLayout.tsx
+++ ui/components/4-Layouts/ListLayout/CardListLayout/CardListLayout.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./CardListLayout.module.css";
 import { ProjectCard } from "../../../2-molecules/ProjectCard";
 import { useRouter } from "next/router";
-import { Container } from "../../Container";
 
 export interface CardListLayoutProps {
   projects: Models.Project[];
@@ -11,13 +10,11 @@ export interface CardListLayoutProps {
 export const CardListLayout: React.FC<CardListLayoutProps> =  ({ projects }) => {
   const router = useRouter();
 
-  console.log(projects[0].projectPictures[0].url)
-
   const handleClick = (slug: string) => {
     router.push(`/projects/${slug}`);
   };
   return (
-   <>
+   <div className={styles.CardListLayout___container}>
    {projects.map((item, index: number) => {
             return (
               <div key={index} className={styles.listItem}>
@@ -30,7 +27,7 @@ export const CardListLayout: React.FC<CardListLayoutProps> =  ({ projects }) => 
               </div>
             );
           })}
-   </>
+   </div>
       
         
           
