01347a6de7df41d5ea586d3105c0e4bb80b15abb
AlubuArt <jc@jcvisueldesign.dk>
2022-01-03 21:41:52 +0100

    calls to YT API

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,39 @@
 import { NextPage } from "next";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Button } from "../ui/components/1-atoms/Button";
 import UnderConstruction from "../ui/components/3-organisms/UnderConstruction/UnderConstruction";
+import { getVideos } from "../lib/youtube";
+import { Heading } from "../ui/components/1-atoms/Heading";
+
+interface video {
+    snippet: { title: string };
+}
 
 const AboutPage: NextPage = () => {
+    const [videos, setVideos] = useState<Array<video>>([]);
+
+    useEffect(() => {
+        const video = async () => {
+            setVideos(await getVideos());
+        };
+        video();
+    }, []);
+
+    const handleCLick = () => {
+        console.log(videos);
+    };
+
     return (
-        
+        <>
             <UnderConstruction pageTitle={"about"} />
-        
+            <Button onClick={handleCLick}>{"try me"}</Button>
+            <Heading type={"h3"} tag={"h3"}>
+                {"My recent viewed YT videos"}
+            </Heading>
+            {videos.map((video, index) => {
+                return <p key={index}>{video.snippet.title}</p>;
+            })}
+        </>
     );
 };
 
--- types/environment.d.ts
+++ types/environment.d.ts
@@ -2,6 +2,7 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             EMAIL_API_KEY: string;
+            YOUTUBE_API_KEY: string;
             NODE_ENV: 'development' | 'production';
             PORT?: string;
             PWD: string;
