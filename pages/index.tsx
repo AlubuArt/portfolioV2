75a01dc304881f435a3f757cf25819b65503db15
AlubuArt <jc@jcvisueldesign.dk>
2022-06-13 07:28:30 +0200

    added graphcms call to hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,38 @@
-import React from "react";
-import type { NextPage, GetStaticProps } from "next";
-import { Hero } from "../ui/components/3-organisms/Hero";
-import { getHeroContent } from "../lib/graphcms";
-import HeroV2 from "../ui/components/3-organisms/Hero/HeroV2/HeroV2";
-import { Container } from "../ui/components/4-Layouts/Container";
+import React from 'react';
+import type { NextPage, GetStaticProps } from 'next';
+import { Hero } from '../ui/components/3-organisms/Hero';
+import { getHeroContent } from '../lib/graphcms';
+import HeroV2 from '../ui/components/3-organisms/Hero/HeroV2/HeroV2';
+import { Container } from '../ui/components/4-Layouts/Container';
 
 export interface HomePageProps {
-    content: {
-        heroes: [
-            {
-                header: string;
-                subheader: { text: string };
-                heroImage: { url: string };
-            }
-        ];
-    };
+  content: {
+    header: string;
+    subheader: { text: string };
+    heroImage: { url: string };
+  };
 }
 
 const Home: NextPage<HomePageProps> = ({ content }) => {
-    return (
-        <>
-            <Container type={"hero"}>
-                <HeroV2 heroTitle={""} heroSubtitle={""} onClick={""} />
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <Container type={'hero'}>
+        <HeroV2
+          heroTitle={''}
+          heroSubtitle={content.subheader.text}
+          onClick={''}
+        />
+      </Container>
+    </>
+  );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-    const content = await getHeroContent();
-    return {
-        props: { content },
-        revalidate: 60,
-    };
+  const content = await getHeroContent();
+  return {
+    props: { content },
+    revalidate: 60,
+  };
 };
 
 export default Home;
