186f22d5145e78746b440ddb0b1933a61a229b06
AlubuArt <jc@jcvisueldesign.dk>
2022-07-05 09:55:45 +0200

    added seo tag component

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // Change your rules accordingly to your coding style preferences.
+    // https://prettier.io/docs/en/options.html
+    trailingComma: 'es5',
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 2,
+    endOfLine: 'auto',
+    semi: true,
+  };
--- ui/components/4-Layouts/PageLayout/PageLayout.tsx
+++ ui/components/4-Layouts/PageLayout/PageLayout.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Head from 'next/head';
 import { Header } from '../../2-molecules/Header';
 import Footer from '../../3-organisms/Footer/Footer';
 import styles from './PageLayout.module.css';
@@ -18,18 +17,12 @@ export const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
   ];
 
   return (
-  
-    
     <div className={styles.PageContent}>
       <Header menuItems={menuItems} />
-      <div className={styles.body}>
-
-      {children}
-      </div>
+      <div className={styles.body}>{children}</div>
       <div className={styles.footer}>
         <Footer />
       </div>
     </div>
-
   );
 };
