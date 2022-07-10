777bddbb164af727d90e1876c81b514c917ad56a
AlubuArt <jc@jcvisueldesign.dk>
2022-07-10 08:29:28 +0200

    added eslint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,6 @@
-module.exports = {
-    // Change your rules accordingly to your coding style preferences.
-    // https://prettier.io/docs/en/options.html
-    trailingComma: 'es5',
-    singleQuote: true,
-    printWidth: 120,
-    tabWidth: 2,
-    endOfLine: 'auto',
-    semi: true,
-  };
+export const trailingComma = 'es5';
+export const singleQuote = true;
+export const printWidth = 120;
+export const tabWidth = 2;
+export const endOfLine = 'auto';
+export const semi = true;
